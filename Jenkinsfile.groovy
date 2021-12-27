a04e80a0db47c8ade3b3f239fb0d9091b7858ea1
aslam711 <58959968+aslam711@users.noreply.github.com>
2021-12-27 10:59:38 +0900

    Removed stages from the file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,32 +7,32 @@ pipeline {
           } 
       }
     }
-    stages {
+    
       stage('build') {
           steps {
               sh ' echo "this is my build step" '
           } 
       }
-    }
-    stages {
+    
+    
       stage('test') {
           steps {
               sh ' This is my test" '
           } 
       }
-    }
-    stages {
+    
+    
       stage('LocalBuild') {
           steps {
               sh ' echo " this my local build" '
           } 
       }
-    }
-    stages {
+    
+    
       stage('push') {
           steps {
               sh ' this is my artifact push" '
           } 
-      }
+      
     }
 }
