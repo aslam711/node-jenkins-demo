4171b01dd4dcbe34b836ed5b833adb8a98be0fff
aslam711 <58959968+aslam711@users.noreply.github.com>
2021-12-27 11:07:00 +0900

    updated - closing section

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,35 +4,29 @@ pipeline {
       stage('build') {
           steps {
               sh ' echo "my home" '
-          } 
-      }
-    }
-    
+            }
+        }
       stage('build') {
           steps {
               sh ' echo "this is my build step" '
           } 
-      }
-    
-    
+        }
       stage('test') {
           steps {
               sh ' This is my test" '
           } 
-      }
-    
-    
+        }
       stage('LocalBuild') {
           steps {
               sh ' echo " this my local build" '
           } 
       }
-    
-    
       stage('push') {
           steps {
               sh ' this is my artifact push" '
-          } 
+            } 
       
+        }
     }
+
 }
